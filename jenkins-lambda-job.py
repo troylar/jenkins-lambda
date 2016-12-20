f284f9d8d335790304c5b9ed5a0f9c0c6ee94748
Troy Larson <troylar@gmail.com>
2016-12-20 17:21:52 -0500

    Update default values from config file

--- jenkins-lambda-job.py
+++ jenkins-lambda-job.py
@@ -1,14 +1,33 @@
 import yaml
 import argparse
 import jenkins
+from xml.etree import ElementTree as et
+
+def set_default_value(config, parameter, default_value):
+   parameter_definitions = config.findall('.//parameterDefinitions/hudson.model.StringParameterDefinition')
+  
+   for node in parameter_definitions:
+      name = node.findall('.//name')
+      is_match=False
+      for e in name:
+         if (e.text == parameter):
+            is_match=True
+      if not is_match:
+         continue
+
+      for child in node.getiterator():
+         if(child.tag=='defaultValue'):
+            child.text = default_value
+
 
 parser = argparse.ArgumentParser(description="Create python lambda jobs in Jenkins")
 parser.add_argument('--configyaml', help='full path of the --configyaml')
 parser.add_argument('--jobname', help='name of the job')
 parser.add_argument('--parameters', help='job custom parameters')
-parser.add_argument('command', nargs='?', help='run,info,create')
+parser.add_argument('command', nargs='?', help='run,update')
 args = parser.parse_args()
 config={}
+job_configs={}
 username=""
 password=""
 url=""
@@ -18,6 +37,7 @@ with open(args.configyaml, 'r') as stream:
         username=config["auth"]["username"]
         password=config["auth"]["password"]
         url=config["jenkins_url"]
+        job_configs=config["jobs"]
     except yaml.YAMLError as exc:
         print(exc)
 server = jenkins.Jenkins(url, username=username, password=password)
@@ -27,14 +47,16 @@ if args.command=='run':
    server.build_job(args.jobname, json.loads(args.parameters))
    quit()
 
-if args.command=='info':
-   job = server.get_job_config(args.jobname)
-   print(job)
+if args.command=='update':
+   for job_config in job_configs:
+      job = server.get_job_config(job_config["name"])
+      tree = et.fromstring(job)
+      print "Updating %s" % (job_config["name"])
+      print job_config
+      for parameter in job_config["parameters"]:
+         set_default_value(tree, parameter["name"], parameter["value"])
+   print et.tostring(tree, encoding='utf8', method='xml')
    quit()
 
 print "Invalid option -- nothing to do"
 
-#plugins = server.get_plugins()
-#for plugin in plugins.keys():
-#   info = server.get_plugin_info(plugin[0])
-#   print info 
