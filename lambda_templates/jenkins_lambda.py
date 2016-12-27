5045fd3df1d7909810fbce724bb8f50445309abf
Troy Larson <troylar@gmail.com>
2016-12-27 15:57:14 -0500

    Unit tests are working

--- lambda_templates/jenkins_lambda.py
+++ lambda_templates/jenkins_lambda.py
@@ -3,28 +3,53 @@ import sys
 import argparse
 import jenkins
 import constants
+import exceptions
 from xml.etree import ElementTree as et
 
-def set_default_value(config, parameter, default_value):
-   parameter_definitions = config.findall('.//parameterDefinitions/hudson.model.StringParameterDefinition')
-  
-   for node in parameter_definitions:
-      name = node.findall('.//name')
-      is_match=False
-      for e in name:
-         if (e.text == parameter):
-            is_match=True
-      if not is_match:
-         continue
+class JenkinsLambda(object):
 
-      for child in node.getiterator():
-         if(child.tag=='defaultValue'):
-            child.text = default_value
+	def __init__(self, url, username, password):
+		self.jenkins_server = jenkins.Jenkins(url, username, password)
+
+	def set_default_value(self, config, parameter, default_value):
+	   parameter_definitions = config.findall('.//parameterDefinitions/hudson.model.StringParameterDefinition')
+	  
+	   for node in parameter_definitions:
+	      name = node.findall('.//name')
+	      is_match=False
+	      for e in name:
+	         if (e.text == parameter):
+	            is_match=True
+	      if not is_match:
+	         continue
+
+	      for child in node.getiterator():
+	         if(child.tag=='defaultValue'):
+	            child.text = default_value
+
+	def create_folder_name(self, job_name):
+	   return ('%s' % (job_name))
+
+	def does_job_exist(self, job_name):
+	   print self.jenkins_server.get_job_info(job_name)
+	   return (self.jenkins_server.get_job_info(job_name) is not None)
+
+	def create_job(self, job_name):
+	   full_folder_name = self.create_folder_name(job_name)
+	   if (self.does_job_exist(full_folder_name)):
+	      raise exceptions.JenkinsException ("Unable to create job: Folder %s already exists" % (full_folder_name))
+
+	   print 'Creating folder %s' % (full_folder_name)
+	   self.jenkins_server.create_job('%s' % (full_folder_name), constants.EMPTY_FOLDER_XML)
 
 def throw_argument_error(parser, message, code=2):
    print "ERROR: %s" % (message)
    parser.print_help()
    sys.exit(code)
+
+def validate_arguments(args):
+   if args.command not in ['create', 'update', 'run']:
+      throw_argument_error (parser, "Invalid command", constants.ARGPARSE_ERROR_INVALID_COMMAND)
 
 def parse_args(args):
    parser = argparse.ArgumentParser(description="Create python lambda jobs in Jenkins")
@@ -38,18 +63,6 @@ def parse_args(args):
       throw_argument_error(parser, "Invalid arguments", constants.ARGPARSE_ERROR_INVALID_ARGUMENTS)
 
    return parser.parse_args(args)
-
-def validate_arguments(args):
-   if args.command not in ['create', 'update', 'run']:
-      throw_argument_error (parser, "Invalid command", constants.ARGPARSE_ERROR_INVALID_COMMAND)
-
-def create_folder_name(job_name):
-   return ('%s' % (job_name))
-
-def create_job(jenkins_server, job_name):
-   full_folder_name = create_folder_name(job_name)
-   print 'Creating folder %s' % (full_folder_name)
-   jenkins_server.create_job('%s' % (full_folder_name), constants.EMPTY_FOLDER_XML)
 
 def read_config(config_yaml):
    with open(config_yaml, 'r') as stream:
@@ -65,10 +78,10 @@ def main():
    args = parse_args(sys.argv[1:])
    validate_arguments(args)
    config = read_config(args.configyaml)
-   server = jenkins.Jenkins(config["jenkins_url"], username=config["auth"]["username"], password=config["auth"]["password"])
+   jenkins_lambda = JenkinsLambda (config["jenkins_url"], username=config["auth"]["username"], password=config["auth"]["password"])
 
    if args.command=='create':
-      create_job(server, args.jobname)
+      jenkins_lambda.create_job(args.jobname)
 
    if args.command=='run':
       print('Running job %s' % (args.jobname))
--- tests/test_jenkins_lambda.py
+++ tests/test_jenkins_lambda.py
@@ -3,10 +3,14 @@ import jenkins
 from mock import MagicMock, patch
 import sys
 import os
+import exceptions
+import json
 sys.path.insert(0, os.path.abspath('..'))
 
 from lambda_templates import constants
+from lambda_templates import exceptions
 from lambda_templates import jenkins_lambda
+from lambda_templates.jenkins_lambda import JenkinsLambda
 
 class CommandLineTestCase(unittest.TestCase):
 
@@ -29,18 +33,26 @@ class CommandLineTestCase(unittest.TestCase):
 
 class JenkinsTestCase(unittest.TestCase):
 
+   @patch.object(jenkins.Jenkins, 'jenkins_open')
    @patch.object(jenkins.Jenkins, 'create_job')
-   def test_create_job_creates_folder(self, create_job_mock):
-      create_job_mock.return_value=None
-      jenkins_mock = jenkins.Jenkins("test", "test", "test")
-      jenkins_lambda.create_job(jenkins_mock, "test_job_name")
+   @patch.object(jenkins.Jenkins, 'get_job_info')
+   def test_create_job_creates_folder(self, get_job_info_mock, create_job_mock, jenkins_mock):
+      jenkins_mock.return_value=None
+      get_job_info_mock.return_value=None
+      jenkins_lambda = JenkinsLambda("a","b","c")
+      jenkins_lambda.create_job("test_job_name")
       create_job_mock.assert_called_with("test_job_name", constants.EMPTY_FOLDER_XML)
 
-   @patch.object(jenkins.Jenkins, 'create_job')
-   def test_gracefully_handle_error_if_folder_already_exist(self, create_job_mock):
-      jenkins_mock = jenkins.Jenkins("test", "test", "test")
-      create_job_mock.side_effect = jenkins.JenkinsException('job[%s] already exists % (name)')
-      jenkins_lambda.create_job(jenkins_mock, "test_job_name")
+   @patch.object(jenkins.Jenkins, 'jenkins_open')
+   @patch.object(jenkins.Jenkins, 'get_job_info')
+   def test_gracefully_handle_error_if_folder_already_exist(self, get_job_info_mock, jenkins_mock):
+      jenkins_mock.return_value=None
+      get_job_info_mock.return_value=json.loads('{"job": "test"}')
+      jenkins_lambda = JenkinsLambda("a","b","c")
+      with self.assertRaises(exceptions.JenkinsException) as context:
+         jenkins_lambda.create_job("test_job_name")
+      print context.exception
+      self.assertTrue("exists" not in context.exception)
 
 if __name__ == '__main__':
    unittest.main()
